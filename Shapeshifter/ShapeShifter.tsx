76a72af4121e82b57e4e0a9a19fa4584749506f9
Brendon <brendon.colburn@gmail.com>
2022-06-20 06:15:08 -0400

    latest changes attempt to fix datepicker

--- Shapeshifter/ShapeShifter.tsx
+++ Shapeshifter/ShapeShifter.tsx
@@ -3,7 +3,7 @@ import { TextField, DatePicker } from '@fluentui/react';
 
 export interface IShapeShifterProps {
   label?: string;
-  onChange: (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string | null) => void
+  onChange: (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string | null) => void 
   controlType: ComponentFramework.PropertyTypes.EnumProperty<"TextField" | "Date">;
   default: string | undefined;
 }
@@ -13,7 +13,7 @@ export class ShapeShifter extends React.Component<IShapeShifterProps> {
   public render(): React.ReactNode {
     let el = (this.props.controlType.raw == "TextField") ?
       <TextField onChange={this.props.onChange} label={this.props.label} defaultValue={this.props.default}></TextField> :
-      <DatePicker label={this.props.label}></DatePicker>
+      <DatePicker label={this.props.label} defaultValue={this.props.default}></DatePicker>
     return el
   }
 }
